2476091a68584c753a5b1c8a21b39fc2f6145a8a
nonk123 <me@nonk.dev>
2025-06-24 18:50:11 +0300

    shitwise shitting???

--- src/gen.c
+++ src/gen.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "yyjson.h"
+
 #define BUF_MAX (2048)
 #define ARGS_MAX (128)
 
@@ -22,92 +24,78 @@ int main(int argc, char* argv[]) {
     if (argc != 4)
         return EXIT_FAILURE;
 
-    FILE *input = fopen(argv[3], "rt"), *hOutput = fopen(argv[1], "wt"), *cOutput = fopen(argv[2], "wt");
-    if (input == NULL || hOutput == NULL || cOutput == NULL)
+    FILE *hOutput = fopen(argv[1], "wt"), *cOutput = fopen(argv[2], "wt");
+    if (hOutput == NULL || cOutput == NULL)
+        return EXIT_FAILURE;
+
+    yyjson_read_flag flg = YYJSON_READ_ALLOW_COMMENTS | YYJSON_READ_ALLOW_TRAILING_COMMAS;
+    yyjson_read_err err;
+    yyjson_doc* doc = yyjson_read_file(argv[3], flg, NULL, &err);
+    if (doc == NULL)
         return EXIT_FAILURE;
 
     fprintf(hOutput, "#pragma once\n\n");
-    fprintf(hOutput, "#include \"caulkSteamTypes.h\"\n\n");
+    fprintf(cOutput, "#include \"__gen.h\"\n\n");
 
-    fprintf(cOutput, "#include \"caulkSteamTypes.h\"\n\n");
+    yyjson_val* obj = yyjson_doc_get_root(doc);
+    yyjson_arr_iter iter;
 
-    char line[BUF_MAX];
-    while (fgets(line, BUF_MAX, input) != NULL) {
-        if (strstr(line, "S_API ") == NULL)
-            continue;
-        const char* sub = line;
+    yyjson_val* struc = NULL;
+    yyjson_val* structs = yyjson_obj_get(obj, "structs");
+    yyjson_arr_iter_init(structs, &iter);
+    while ((struc = yyjson_arr_iter_next(&iter)) != NULL) {
+        const char* name = yyjson_get_str(yyjson_obj_get(struc, "struct"));
+        fprintf(hOutput, "typedef struct %s %s;\n", name, name);
+    }
+    printl(hOutput, "\n");
 
-        while (*++sub != ' ') {}
-        const char* typeStart = sub + 1;
+    struc = NULL;
+    structs = yyjson_obj_get(obj, "callback_structs");
+    yyjson_arr_iter_init(structs, &iter);
+    while ((struc = yyjson_arr_iter_next(&iter)) != NULL) {
+        const char* name = yyjson_get_str(yyjson_obj_get(struc, "struct"));
+        fprintf(hOutput, "typedef struct %s %s;\n", name, name);
+    }
+    printl(hOutput, "\n");
 
-        while (*++sub != '(') {}
-        const char *typeAndNameEnd = sub - 1, *argsStart = sub;
+    yyjson_val* typedf = NULL;
+    yyjson_val* typedefs = yyjson_obj_get(obj, "typedefs");
+    yyjson_arr_iter_init(typedefs, &iter);
+    while ((typedf = yyjson_arr_iter_next(&iter)) != NULL) {
+        const char* name = yyjson_get_str(yyjson_obj_get(typedf, "typedef"));
+        const char* type = yyjson_get_str(yyjson_obj_get(typedf, "type"));
+        printl(hOutput, "typedef ");
 
-        while (*++sub != '\n') {}
-        const char* argsEnd = sub - 2;
-
-        sub = typeAndNameEnd;
-        while (*--sub != ' ') {}
-
-        const char *name = sub + 1, *type = typeStart, *args = argsStart;
-        size_t nameLen = typeAndNameEnd - name + 1, typeLen = name - type - 1, argsLen = argsEnd - argsStart + 1;
-
-        for (char* c = (char*)argsStart; c != argsEnd; c++)
-            if (*c == '&')
-                *c = '*';
-
-        prints(hOutput, type, typeLen);
-        printl(hOutput, " caulk__");
-        prints(hOutput, name, nameLen);
-        prints(hOutput, args, argsLen);
+        char* offset = NULL;
+        if ((offset = strstr(type, "(*)")) != NULL) {
+            prints(hOutput, type, offset + 2 - type);
+            fprintf(hOutput, "%s%s", name, offset + 2);
+        } else if ((offset = strstr(type, "[")) != NULL) {
+            prints(hOutput, type, offset - 1 - type);
+            fprintf(hOutput, " %s%s", name, offset);
+        } else
+            fprintf(hOutput, "%s %s", type, name);
         printl(hOutput, ";\n");
+    }
+    printl(hOutput, "\n");
 
-        struct arg parsedArgs[ARGS_MAX];
-        for (size_t i = 0; i < ARGS_MAX; i++)
-            parsedArgs[i].name = NULL;
+    yyjson_val* enm = NULL;
+    yyjson_val* enums = yyjson_obj_get(obj, "enums");
+    yyjson_arr_iter_init(structs, &iter);
+    while ((enm = yyjson_arr_iter_next(&iter)) != NULL) {
+        const char* name = yyjson_get_str(yyjson_obj_get(enm, "enumname"));
+        fprintf(hOutput, "enum %s {\n", name);
+    }
+    printl(hOutput, "\n");
 
-        const char* curArg = argsStart + 1;
-        size_t argsParsed = 0;
-        for (;;) {
-            while (*curArg == ' ' || *curArg == ',')
-                curArg++;
-            if (*curArg == ')')
-                break;
-
-            const char* argEnd = curArg;
-            while (*argEnd != ',' && *argEnd != ')')
-                argEnd++;
-            if (*argEnd == ')')
-                argEnd--;
-
-            curArg = argEnd;
-            while (*--curArg == ' ') {}
-            while (*--curArg != ' ') {}
-
-            parsedArgs[argsParsed].name = curArg + 1;
-            parsedArgs[argsParsed].len = argEnd - parsedArgs[argsParsed].name;
-
-            argsParsed++;
-            curArg = argEnd;
-        }
-
-        prints(cOutput, type, typeLen);
-        printl(cOutput, " caulk__");
-        prints(cOutput, name, nameLen);
-        prints(cOutput, args, argsLen);
-        printl(cOutput, " {\n\treturn ");
-        prints(cOutput, name, nameLen);
-        printl(cOutput, "(");
-        for (size_t i = 0; i < argsParsed; i++) {
-            prints(cOutput, parsedArgs[i].name, parsedArgs[i].len);
-            if (i != argsParsed - 1)
-                printl(cOutput, ", ");
-        }
-        printl(cOutput, ");\n");
-        printl(cOutput, "}\n\n");
+    struc = NULL;
+    structs = yyjson_obj_get(obj, "structs");
+    yyjson_arr_iter_init(structs, &iter);
+    while ((struc = yyjson_arr_iter_next(&iter)) != NULL) {
+        yyjson_obj_get(struc, "struct");
     }
 
-    fclose(input);
+    yyjson_doc_free(doc);
     fclose(hOutput);
     fclose(cOutput);
 
