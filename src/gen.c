59b2b22b22c232a5274b5e46e6fe88831355df43
nonk123 <me@nonk.dev>
2025-06-26 16:04:32 +0300

    `__gen.h` compiles!!!!!!

--- src/gen.c
+++ src/gen.c
@@ -35,6 +35,9 @@ int main(int argc, char* argv[]) {
         return EXIT_FAILURE;
 
     fprintf(hOutput, "#pragma once\n\n");
+    fprintf(hOutput, "#include <stdint.h>\n\n");
+    fprintf(hOutput, "typedef uint32_t enum32_t;\n\n");
+
     fprintf(cOutput, "#include \"__gen.h\"\n\n");
 
     yyjson_val* obj = yyjson_doc_get_root(doc);
@@ -48,6 +51,28 @@ int main(int argc, char* argv[]) {
         const char* type = yyjson_get_str(yyjson_obj_get(cnst, "consttype"));
         const char* value = yyjson_get_str(yyjson_obj_get(cnst, "constval"));
         fprintf(hOutput, "#define %s ((%s)(%s))\n", name, type, value);
+    }
+    printl(hOutput, "\n");
+
+    yyjson_val* enm = NULL;
+    yyjson_val* enums = yyjson_obj_get(obj, "enums");
+    yyjson_arr_iter_init(enums, &iter);
+    while ((enm = yyjson_arr_iter_next(&iter)) != NULL) {
+        const char* name = yyjson_get_str(yyjson_obj_get(enm, "enumname"));
+        fprintf(hOutput, "typedef enum32_t %s;\n", name);
+        fprintf(hOutput, "enum %s {\n", name);
+
+        yyjson_val* val = NULL;
+        yyjson_val* values = yyjson_obj_get(enm, "values");
+        yyjson_arr_iter val_iter;
+        yyjson_arr_iter_init(values, &val_iter);
+        while ((val = yyjson_arr_iter_next(&val_iter)) != NULL) {
+            name = yyjson_get_str(yyjson_obj_get(val, "name"));
+            const char* value = yyjson_get_str(yyjson_obj_get(val, "value"));
+            fprintf(hOutput, "    %s = %s,\n", name, value);
+        }
+
+        fprintf(hOutput, "};\n\n");
     }
     printl(hOutput, "\n");
 
@@ -87,27 +112,6 @@ int main(int argc, char* argv[]) {
         } else
             fprintf(hOutput, "%s %s", type, name);
         printl(hOutput, ";\n");
-    }
-    printl(hOutput, "\n");
-
-    yyjson_val* enm = NULL;
-    yyjson_val* enums = yyjson_obj_get(obj, "enums");
-    yyjson_arr_iter_init(enums, &iter);
-    while ((enm = yyjson_arr_iter_next(&iter)) != NULL) {
-        const char* name = yyjson_get_str(yyjson_obj_get(enm, "enumname"));
-        fprintf(hOutput, "enum %s {\n", name);
-
-        yyjson_val* val = NULL;
-        yyjson_val* values = yyjson_obj_get(enm, "values");
-        yyjson_arr_iter val_iter;
-        yyjson_arr_iter_init(values, &val_iter);
-        while ((val = yyjson_arr_iter_next(&val_iter)) != NULL) {
-            name = yyjson_get_str(yyjson_obj_get(val, "name"));
-            const char* value = yyjson_get_str(yyjson_obj_get(val, "value"));
-            fprintf(hOutput, "    %s = %s,\n", name, value);
-        }
-
-        fprintf(hOutput, "};\n\n");
     }
     printl(hOutput, "\n");
 
